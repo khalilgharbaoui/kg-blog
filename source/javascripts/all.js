05ec432d8cb1799a5c14435b45410f9d4753bbbf
khalilgharbaoui <kaygeee@gmail.com>
2016-04-04 16:33:38 +0200

    added flip card to my work portfolio

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     percentPosition: true,
     transitionDuration: '0.5s',
     initLayout: true,
-    columnWidth: 321,
+    columnWidth: 1,
     resize: true
 
   });
@@ -98,7 +98,7 @@ $(document).ready(function() {
     window.location.pathname === '/my-work/' ||
     window.location.pathname === '/blog/'
   ) {
-    
+
       $("#back-button").hide();
       $("#about-me-button").show();
       $("#my-work-button").show();
@@ -113,6 +113,75 @@ $(document).ready(function() {
       $("#contact-me-button").hide();
 
     }
+
+
+
+
+    // portfolio flipper start
+
+    function flipThis(e) {
+    var $this;
+    if($(e.currentTarget).prop('tagName')=="BUTTON")
+        $this = $(this).closest('.grid-item-large');
+    else
+        $this = $(this);
+    console.log($this);
+    var card = $this.find('.flipcard');
+    var front = $this.find('.flipcard-front');
+    var back = $this.find('.flipcard-back');
+    var tallerHight = Math.max(front.height(), back.height()) + 'px';
+    var visible = front.hasClass('ms-front-flipped') ? back : front;
+    var invisible = front.hasClass('ms-front-flipped') ? front : back;
+    var hasTransitioned = false;
+    var onTransitionEnded = function () {
+        hasTransitioned = true;
+        card.css({
+            'min-height': '0px'
+        });
+        visible.css({
+            display: 'none',
+        });
+        // setting focus is important for keyboard users who might otherwise
+        // interact with the back of the card once it is flipped.
+        invisible.css({
+            position: 'relative',
+            display: 'inline-block',
+        }).find('button:first-child,a:first-child').focus();
+    }
+
+    // portfolio flipper end
+
+
+    // this is bootstrap support, but you can listen to the browser-specific
+    // events directly as well
+    card.one($.support.transition.end, onTransitionEnded);
+
+    // for browsers that do not support transitions, like IE9
+    setTimeout(function () {
+        if (!hasTransitioned) {
+            onTransitionEnded.apply();
+        }
+    }, 2000);
+
+    invisible.css({
+        position: 'absolute',
+        display: 'inline-block'
+    });
+
+    card.css('min-height', tallerHight);
+    // the IE way: flip each face of the card
+    front.toggleClass('ms-front-flipped');
+    back.toggleClass('ms-back-flipped');
+    // the webkit/FF way: flip the card
+    card.toggleClass('card-flipped');
+}
+
+$('button.flip-it').click(flipThis);
+$('div.flip-it').click(flipThis);
+
+
+
+
   });
 
   function fire() {
@@ -150,3 +219,10 @@ $(document).ready(function() {
   $(window).on("scroll", function() {
     $('.social-footer li a').toggleClass('hovered');
   });
+
+  $(document).ready(function() {
+    $('.hover').bind('touchstart touchend', function(e) {
+        e.preventDefault();
+        $(this).toggleClass('hover_effect');
+    });
+});
